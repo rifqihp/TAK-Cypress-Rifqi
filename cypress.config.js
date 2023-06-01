598cc1a465854cf1d3c9433a43a3d2abb0a7d831
rifqihp <50226335+rifqihp@users.noreply.github.com>
2023-06-01 16:04:52 +0700

    Update cypress.config.js
    Perubahan

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,27 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+const browserfy = require("@cypress/browserify-preprocessor");
+
+
+module.exports = defineConfig({
+  env: {
+    baseUrl:"https://itera-qa.azurewebsites.net"
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      let options = browserfy.defaultOptions;
+    options.browserifyOptions.transform[1][1].plugins.push([
+      "module-resolver",
+      {
+        alias: {
+          "@tests": "./tests",
+        },
+      },
+    ]);
+      on("file:preprocessor", browserfy(options));
     },
+    specPattern: 'tests/scenarios/**/*.js',
+    screenshotOnRunFailure:true,
+    video: false
   },
-};
+});
